f7fb875dd8fcfb9a714d87b8cc6769bb4303c9b0
jaddison06 <addison.j@etoncollege.org.uk>
2020-11-15 20:11:43 +0000

    basic client

---
+++ app/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+import { onMount } from 'svelte';
+
+	import Main from './Main.svelte';
+
+	document.title = "shitty chat app";
+
+	//let logged_in = false;
+	let logged_in = true;
+	let name = 'me';
+	function login() {
+		logged_in = true;
+	}
+
+	function inputKeypress(event) {
+		if (event.key === "Enter") { login() }
+	}
+
+	let nameInput;
+	onMount(function() {
+		if (!logged_in) {nameInput.focus(); }
+	});
+
+</script>
+
+{#if !logged_in}
+<p>Name: </p>
+<input bind:value = {name} on:keydown={inputKeypress} type = "text" bind:this = {nameInput}/>
+<button on:click = {login}>Log me in!</button>
+{:else}
+<Main name = {name}/>
+{/if}
---
+++ app/src/Main.svelte
@@ -0,0 +1,149 @@
+<script>
+import { onMount } from "svelte";
+
+
+    export let name;
+    let currentMsg="";
+    let users = ['you', 'them', 'someone else'];
+    let messages = [
+        {
+            sender: name,
+            msg: "wassup"
+        },
+        {
+            sender: "them",
+            msg: "smol pp"
+        },
+        {
+            sender: name,
+            msg: "o sorry"
+        }
+    ];
+
+    function sendMessage() {
+        let message = {
+            sender: name,
+            msg: currentMsg
+        };
+        messages = [...messages, message];
+        currentMsg = "";
+    }
+
+    function sendBoxKeypress(event) {
+        if (event.key === "Enter") { sendMessage(); }
+    }
+
+    let sendBox;
+
+    onMount(function() {
+        sendBox.focus();
+    });
+</script>
+
+<style>
+    #usersSidebar {
+        width: 10%;
+        position: fixed;
+        height: 100%;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        background-color: rgb(49, 49, 49);
+        overflow-x: hidden;
+        padding-top: 20px;
+    }
+    #users {
+        position: absolute;
+        left: 10px;
+    }
+    .me {
+        color:rgb(5, 143, 185);
+    }
+    .otherUser {
+        color:rgb(255, 255, 255);
+    }
+
+    #messageArea {
+        position: fixed;
+        left: 10%;
+        width: 85%;
+        height: 100%;
+    }
+
+    #messages {
+        position: absolute;
+        left: 10%;
+        top: 0%;
+        height: 90%;
+        width: 90%;
+    }
+    .message {
+        position: absolute;
+        font-size: 15px;
+        height: 15px;
+        width: 100%;
+    }
+    .messageSenderNotMe {
+        color:rgb(49, 49, 49);
+    }
+    .messageText {
+        font-style: italic;
+    }
+
+    #messageControls {
+        position: relative;
+        margin: auto;
+        top: 90%;
+        width: 80%;
+        height: 5%;
+    }
+    #messageInput {
+        border-style: solid;
+        border-width: 4px;
+        position: absolute;
+        width: 88%;
+        height: 100%;
+        left: 0;
+    }
+    #messageSend {
+        position: absolute;
+        width: 8%;
+        height: 100%;
+        right: 0;
+    }
+
+</style>
+
+<div id = "usersSidebar">
+    <div id = "users">
+        <p class = "me">{name}</p>
+        {#each users as user}
+        <p class = "otherUser">{user}</p>
+        {/each}
+    </div>
+</div>
+
+<div id = "messageArea">
+    <div id = "messages">
+
+        {#each messages as message}
+        <div class = "message" style = "bottom: {(messages.length - messages.indexOf(message)) * 50}px;">
+
+            {#if message.sender === name}
+            <span class = "me">{message.sender}: </span>
+            {:else}
+            <span class = "messageSenderNotMe">{message.sender}: </span>
+            {/if}
+            
+            <span class = "messageText">{message.msg}</span>
+        </div>
+        {/each}
+        
+    </div>
+
+    <div id = "messageControls">
+        <input bind:value = {currentMsg} type = "text" id = "messageInput" on:keydown={sendBoxKeypress} bind:this = {sendBox}/>
+        <button on:click = {sendMessage} id = "messageSend">Send</button>
+    </div>
+
+</div>
